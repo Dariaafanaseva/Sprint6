dabf9d343b93c0d8e7444f0a60e54ed13d8f3c35
Daria Afanaseva <dariastukalo@yandex.ru>
2024-10-23 05:42:27 +1000

    Добавлены тесты в ветку develop

---
+++ pages/make_order_page.py
@@ -0,0 +1,54 @@
+import allure
+from pages.base_page import BasePage
+from locators.main_page_locators import MainPageLocators
+from locators.make_order_locators import MakeOrderLocators
+
+
+class OrderPage(BasePage):
+    @allure.step('Переходим на страницу самоката')
+    def get(self, url):
+        self.driver.get(url)
+
+    @allure.step('Создаем заказ с кнопкой "Заказать" вверху станицы')
+    def make_order_button_on_header(self, name, surname, address, telephone):
+        self.accept_cookies(MainPageLocators.COOKIE_BUTTON)
+        self.find_element_with_wait(MainPageLocators.MAKE_ORDER_BUTTON_ON_HEADER).click()
+        self.add_text_to_element(MakeOrderLocators.NAME_INPUT_FIELD, name)
+        self.add_text_to_element(MakeOrderLocators.SURNAME_INPUT_FIELD, surname)
+        self.add_text_to_element(MakeOrderLocators.ADDRESS_INPUT_FIELD, address)
+        self.click_to_element(MakeOrderLocators.METRO_INPUT_FIELD)
+        self.find_element_with_wait(MakeOrderLocators.METRO_STATION_OPTION).click()
+        self.add_text_to_element(MakeOrderLocators.TELEPHONE_NUMBER, telephone)
+        self.click_to_element(MakeOrderLocators.NEXT_BUTTON)
+        self.find_element_with_wait(MakeOrderLocators.ABOUT_RENT_HEADER)
+        self.click_to_element(MakeOrderLocators.DELIVERY_DATE)
+        self.add_text_to_element(MakeOrderLocators.DELIVERY_DATE, '25.10.2024')
+        self.click_to_element(MakeOrderLocators.RENTAL_PERIOD_ARROW)
+        self.click_to_element(MakeOrderLocators.RENTAL_PERIOD_TWO_DAYS)
+        self.click_to_element(MakeOrderLocators.MAKE_ORDER_BUTTON)
+        self.find_element_with_wait(MakeOrderLocators.YES_BUTTON).click()
+
+    @allure.step('Создаем заказ с кнопкой "Заказать" внизу станицы')
+    def make_order_button_on_down(self, name, surname, address, telephone):
+        self.accept_cookies(MainPageLocators.COOKIE_BUTTON)
+        self.scroll_to_element(MainPageLocators.TITLE_QUESTIONS)
+        self.find_element_with_wait(MainPageLocators.MAKE_ORDER_BUTTON_ON_DOWN).click()
+        self.add_text_to_element(MakeOrderLocators.NAME_INPUT_FIELD, name)
+        self.add_text_to_element(MakeOrderLocators.SURNAME_INPUT_FIELD, surname)
+        self.add_text_to_element(MakeOrderLocators.ADDRESS_INPUT_FIELD, address)
+        self.click_to_element(MakeOrderLocators.METRO_INPUT_FIELD)
+        self.find_element_with_wait(MakeOrderLocators.METRO_STATION_OPTION).click()
+        self.add_text_to_element(MakeOrderLocators.TELEPHONE_NUMBER, telephone)
+        self.click_to_element(MakeOrderLocators.NEXT_BUTTON)
+        self.find_element_with_wait(MakeOrderLocators.ABOUT_RENT_HEADER)
+        self.click_to_element(MakeOrderLocators.DELIVERY_DATE)
+        self.add_text_to_element(MakeOrderLocators.DELIVERY_DATE, '25.10.2024')
+        self.click_to_element(MakeOrderLocators.RENTAL_PERIOD_ARROW)
+        self.click_to_element(MakeOrderLocators.RENTAL_PERIOD_TWO_DAYS)
+        self.click_to_element(MakeOrderLocators.MAKE_ORDER_BUTTON)
+        self.find_element_with_wait(MakeOrderLocators.YES_BUTTON).click()
+
+
+    @allure.step('Проверяем, что заказ создался')
+    def check_order(self):
+        return self.get_text_from_element(MakeOrderLocators.POPUP_ORDER_PLACED_BUTTON)
---
+++ pages/switch_to_page.py
@@ -0,0 +1,27 @@
+import allure
+from pages.base_page import BasePage
+from locators.switch_locators import SwitchToPageLocators
+from locators.main_page_locators import MainPageLocators
+
+class SwitchToPage(BasePage):
+    @allure.step('Переходим на страницу самоката')
+    def get(self, url):
+        self.driver.get(url)
+
+    @allure.step('Переходим на страницу самоката, при нажатии на лого самоката')
+    def switch_to_scooter_main_page(self):
+        self.accept_cookies(MainPageLocators.COOKIE_BUTTON)
+        self.click_to_element(MainPageLocators.MAKE_ORDER_BUTTON_ON_HEADER)
+        self.find_element_with_wait(SwitchToPageLocators.HEADER_LOGO_SCOOTER).click()
+
+    @allure.step('Переходим на страницу Дзена, при нажатии на лого Яндекс')
+    def switch_to_dzen(self):
+        self.accept_cookies(MainPageLocators.COOKIE_BUTTON)
+        self.find_element_with_wait(SwitchToPageLocators.HEADER_LOGO_YANDEX).click()
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+        self.find_element_with_wait(SwitchToPageLocators.DZEN_SEARCH_BAR_BUTTON)
+
+
+
+
+
